46388ee8a96e53d710d1720c8de6ce8ef17dc6b5
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-05-21 00:28:51 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿char* findLatestTime(char* s) {
+//    int minute = s[3];
+//    printf("%d",minute);
+
+    if(s[0]==63)
+        if((s[1]<50)||(s[1]==63))
+            s[0]=49;
+        else
+            s[0]=48;
+    if(s[1]==63)
+        if(s[0]==49)
+            s[1]=49;
+        else
+            s[1]=57;
+    if(s[3]==63)
+        s[3]=53;
+    if(s[4]==63)
+        s[4]=57;
+//    for(int x = 0; x < len; x++)
+//    {
+//        printf("%c %d\n",s[x], hourOne);
+//    }
+    return s;
+}
